17a00cfe9b60c0e6a6f1fc1707ae03a9c8fa0400
alberto <info@gmaster.eu>
2023-06-08 21:23:57 +0200

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState([ 'One Puch' ]);
+
+    const onAddCategory = ( newCategory ) => {
+        if ( categories.includes( newCategory )) return;
+        setCategories( [ newCategory, ...categories]);
+    }
+
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+            
+            <AddCategory 
+                onNewCategory={ (value) => onAddCategory(value) }
+                /*setCategories={ setCategories }*/
+            />
+
+            {
+                categories.map( category =>
+                        (
+                        <GifGrid 
+                            key={ category } 
+                            category={ category }
+                        />
+                    ))
+            }
+           
+        </>
+    );
+}
